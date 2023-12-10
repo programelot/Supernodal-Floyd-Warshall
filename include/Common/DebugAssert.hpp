6f0b18edbcd0dc26caa677a83cae2ed9d0d5bc43
HyunMo Sung <programelot@gmail.com>
2023-12-10 21:49:41 +0900

    Assert Function in debug mode only

---
+++ include/Common/DebugAssert.hpp
@@ -0,0 +1,10 @@
+// Assert library only actives it when it is on debug mode. //
+// Author : Hyunmo Sung //
+// Contact: programelot@gmail.com //
+
+#ifndef INCLUDE_DEBUGASSERT_HPP
+#define INCLUDE_DEBUGASSERT_HPP
+
+void AssertDebug(const char* file, int line, const char* message, bool test);
+
+#endif
