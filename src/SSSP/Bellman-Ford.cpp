bf2f19001de448e4ac61f43dea92d5e5b868ca8c
HyunMo Sung <programelot@gmail.com>
2023-12-20 12:54:37 +0900

    type fixed

--- src/SSSP/Bellman-Ford.cpp
+++ src/SSSP/Bellman-Ford.cpp
@@ -13,35 +13,33 @@ namespace{
     }
 }
 
-void SSSP(size_t src, const CSRGraph& input_graph, weight_t** distance){
+void SSSP(dataSize_t src, const CSRGraph& input_graph, weight_t* distance){
 
-    size_t size = input_graph.Size();
-    size_t* rowPtr = input_graph.RowPtr();
-    size_t* colIdx = input_graph.ColIdx();
+    dataSize_t size = input_graph.Size();
+    dataSize_t* rowPtr = input_graph.RowPtr();
+    dataSize_t* colIdx = input_graph.ColIdx();
     weight_t* value = input_graph.Value();
 
-    *distance = new weight_t[size];
-    weight_t* result = *distance;
     //Initialize
     for(int i = 0; i < size; ++i){
-        result[i] = kWeightInf;
+        distance[i] = kWeightInf;
     }
-    result[src] = 0;
-    // bool quickExit = false;
-    for(size_t repeat = 0; repeat < size - 1; ++repeat){
-        // if(quickExit) break;//If it is stable, exit it fast
-        // quickExit = true;
-        for(size_t i = 0; i < size; ++i){
+    distance[src] = 0;
+    bool quickExit = false;
+    for(dataSize_t repeat = 0; repeat < size - 1; ++repeat){
+        if(quickExit) break;//If it is stable, exit it fast
+        quickExit = true;
+        for(dataSize_t i = 0; i < size; ++i){
              //If i is not reachable, pass
-            if(result[i] == kWeightInf){ 
+            if(distance[i] == kWeightInf){ 
                 continue;
             }
-            for(size_t j = rowPtr[i]; j < rowPtr[i + 1]; ++j){
-                weight_t oldVal = result[colIdx[j]];
-                weight_t newVal = result[i] + value[j];
+            for(dataSize_t j = rowPtr[i]; j < rowPtr[i + 1]; ++j){
+                weight_t oldVal = distance[colIdx[j]];
+                weight_t newVal = distance[i] + value[j];
                 if(newVal < oldVal){
-                    result[colIdx[j]] = newVal;
-                    // quickExit = false;
+                    distance[colIdx[j]] = newVal;
+                    quickExit = false;
                 }
             }
         }
