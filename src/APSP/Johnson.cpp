beb39ab4f18b347b9afb2e70a144c22092c3d045
HyunMo Sung <programelot@gmail.com>
2023-12-20 12:53:35 +0900

    Johnson bug fix

--- src/APSP/Johnson.cpp
+++ src/APSP/Johnson.cpp
@@ -2,6 +2,10 @@
 // Author : Hyunmo Sung //
 // Contact: programelot@gmail.com //
 
+// Parallelism : O(|V|) //
+// Work complexity : O(|E||V|log|V|) //
+// Time complexity with PRAM : O(|E||V|) //
+
 #include "Common/Type.hpp"
 #include "Graph/CSRGraph.hpp"
 #include "Algorithm/APSP.hpp"
@@ -13,146 +17,130 @@ namespace{
     }
 }
 
-void APSP(const CSRGraph& input_graph, weight_t** distance){
+void APSP(const CSRGraph& input_graph, weight_t* distance){
 
-    size_t size = input_graph.Size();
-    *distance = new weight_t[size * size];
-    weight_t* result = *distance;
+    dataSize_t size = input_graph.Size();
     
-    size_t* rowPtr = input_graph.RowPtr();
-    size_t* colIdx = input_graph.ColIdx();
+    dataSize_t* rowPtr = input_graph.RowPtr();
+    dataSize_t* colIdx = input_graph.ColIdx();
     weight_t* value = input_graph.Value();
     
     //Make argumented graph for Johnson's algorithm
     //Graph that has one more vertex than input graph.
     //This vertex attached to other vertex without cost(in other word, weight is zero)
 
-    size_t nnz = rowPtr[size];
-    size_t size_arg = size + 1;
-    size_t nnz_arg = nnz + size * 2;
-    size_t* rowPtr_Arg = new size_t[size_arg + 1];
-    size_t* colIdx_Arg = new size_t[nnz_arg];
+    dataSize_t nnz = input_graph.NEdge();
+    dataSize_t size_arg = size + 1;
+    dataSize_t nnz_arg = nnz + size * 2;
+    dataSize_t* rowPtr_Arg = new dataSize_t[size_arg + 1];
+    dataSize_t* colIdx_Arg = new dataSize_t[nnz_arg];
     weight_t* value_Arg = new weight_t[nnz_arg];
     
-    for(size_t i = 0; i < size; ++i){
+    for(dataSize_t i = 0; i < size; ++i){
         rowPtr_Arg[i] = rowPtr[i] + i;
-        for(size_t j = rowPtr[i]; j < rowPtr[i + 1]; ++j){
+        for(dataSize_t j = rowPtr[i]; j < rowPtr[i + 1]; ++j){
             colIdx_Arg[j + i] = colIdx[j];
             value_Arg[j + i] = value[j];
         }
         colIdx_Arg[rowPtr[i + 1] + i] = size_arg - 1;
         value_Arg[rowPtr[i + 1] + i] = 0;
     }
-    rowPtr_Arg[size] = rowPtr[size] + size;
+    rowPtr_Arg[size] = nnz + size;
     rowPtr_Arg[size_arg] = nnz_arg;
-    size_t base = rowPtr_Arg[size];
-    for(size_t j = 0; j < size; ++j){
+    dataSize_t base = rowPtr_Arg[size];
+    for(dataSize_t j = 0; j < size; ++j){
         colIdx_Arg[base + j] = j;
         value_Arg[base + j] = 0;
     }
 
     //Bellman ford algorithm from new vertex
-    size_t src_arg = size;
-    weight_t* result_Bellman = new weight_t[size_arg];
+    dataSize_t src_arg = size;
+    weight_t* distance_Bellman = new weight_t[size_arg];
     //Initialize
     for(int i = 0; i < size_arg; ++i){
-        result_Bellman[i] = kWeightInf;
+        distance_Bellman[i] = kWeightInf;
     }
-    result_Bellman[src_arg] = 0;
-    // bool quickExit = false;
-    for(size_t repeat = 0; repeat < src_arg - 1; ++repeat){
-        // if(quickExit) break;
-        // quickExit = true;
-        for(size_t i = 0; i < size_arg; ++i){
+    distance_Bellman[src_arg] = 0;
+    bool quickExit = false;
+    for(dataSize_t repeat = 0; repeat < src_arg - 1; ++repeat){
+        if(quickExit) break;
+        quickExit = true;
+        for(dataSize_t i = 0; i < size_arg; ++i){
              //If i is not reachable, pass
-            if(result_Bellman[i] == kWeightInf){ 
+            if(distance_Bellman[i] == kWeightInf){ 
                 continue;
             }
-            for(size_t j = rowPtr_Arg[i]; j < rowPtr_Arg[i + 1]; ++j){
-                weight_t oldVal = result_Bellman[colIdx_Arg[j]];
-                weight_t newVal = result_Bellman[i] + value_Arg[j];
+            for(dataSize_t j = rowPtr_Arg[i]; j < rowPtr_Arg[i + 1]; ++j){
+                weight_t oldVal = distance_Bellman[colIdx_Arg[j]];
+                weight_t newVal = distance_Bellman[i] + value_Arg[j];
                 if(newVal < oldVal){
-                    result_Bellman[colIdx_Arg[j]] = newVal;
-                    // quickExit = false;
+                    distance_Bellman[colIdx_Arg[j]] = newVal;
+                    quickExit = false;
                 }
             }
         }
     }
-    // printf("=========================\n");
-    // for(size_t i = 0; i < size_arg; ++i){
-    //     printf("%f ", result_Bellman[i]);
-    // }printf("\n");
-    // printf("=========================\n");
 
     //Remove negative edge
     weight_t* value_positive = new weight_t[nnz];
-    for(size_t i = 0; i < size; ++i){
-        for(size_t j = rowPtr[i]; j < rowPtr[i + 1]; ++j){
-            value_positive[j] = result_Bellman[i] + value[j] - result_Bellman[colIdx[j]];
+    for(dataSize_t i = 0; i < size; ++i){
+        for(dataSize_t j = rowPtr[i]; j < rowPtr[i + 1]; ++j){
+            value_positive[j] = distance_Bellman[i] + value[j] - distance_Bellman[colIdx[j]];
         }
     }
 
     //Dijkstra algorithm with positive edges
-
-    BinaryHeapTicket** ticket = new BinaryHeapTicket*[size];
-    bool* visited = new bool[size];
-    BinaryHeap heap;
-    
-    for(size_t src = 0; src < size; ++src)
+    #pragma omp parallel
     {
-        heap.clear();
-        //Initialize
-        for(int i = 0; i < size; ++i){
-            result[src * size + i] = kWeightInf;
-            visited[i] = false;
-            ticket[i] = nullptr;
-        }
-        result[src * size + src] = 0;
-        visited[src] = true;
-        for(size_t i = rowPtr[src]; i < rowPtr[src + 1]; ++i){
-            HeapNode data;
-            data.index = colIdx[i];
-            data.value = value_positive[i];
-            ticket[colIdx[i]] = heap.Insert(data);
-        }
-        while(!heap.isEmpty()){
-            HeapNode top = heap.Pop();
-            size_t selected = top.index;
-            visited[selected] = true;
-            result[src * size + selected] = top.value;
-            for(size_t i = rowPtr[selected]; i < rowPtr[selected + 1]; ++i){
-                if(visited[colIdx[i]]) continue;
-                if(ticket[colIdx[i]] == nullptr){
-                    HeapNode data;
-                    data.index = colIdx[i];
-                    data.value = value_positive[i] + result[src * size  + selected];
-                    ticket[colIdx[i]] = heap.Insert(data);
-                }
-                else{
-                    HeapNode data;
-                    data.index = colIdx[i];
-                    data.value = value_positive[i] + result[src * size  + selected];
-                    HeapNode oldData = heap.Get(ticket[colIdx[i]]);
-                    if(data.value < oldData.value){
-                        heap.Update(ticket[colIdx[i]], data);
+        #pragma omp for
+        for(dataSize_t src = 0; src < size; ++src)
+        {
+            bool* visited = new bool[size];
+            BinaryHeap heap(size);
+
+            //Initialize
+            for(int i = 0; i < size; ++i){
+                distance[src * size + i] = kWeightInf;
+                visited[i] = false;
+            }
+            distance[src * size + src] = 0;
+            visited[src] = true;
+            for(dataSize_t i = rowPtr[src]; i < rowPtr[src + 1]; ++i){
+                if(colIdx[i] == src) continue;
+                heap.Insert(colIdx[i], value_positive[i]);
+            }
+            while(!heap.IsEmpty()){
+                HeapNode top = heap.Pop();
+                dataSize_t selected = top.index;
+                visited[selected] = true;
+                distance[src * size + selected] = top.value;
+                for(dataSize_t i = rowPtr[selected]; i < rowPtr[selected + 1]; ++i){
+                    if(visited[colIdx[i]]) continue;
+                    if(heap.Inserted(colIdx[i])){
+                        weight_t newValue = value_positive[i] + distance[src * size  + selected];
+                        weight_t oldData = heap.Get(colIdx[i]);
+                        if(newValue < oldData){
+                            heap.Update(colIdx[i], newValue);
+                        }
+                    }
+                    else{
+                        heap.Insert(colIdx[i], value_positive[i] + distance[src * size  + selected]);
                     }
                 }
             }
+            delete[] visited;
         }
     }
 
     //Get correct distance from algorithm
     
-    for(size_t i = 0; i < size; ++i){   
-        for(size_t j = 0; j < size; ++j){
-            result[i * size + j] += result_Bellman[j] - result_Bellman[i];         
+    for(dataSize_t i = 0; i < size; ++i){   
+        for(dataSize_t j = 0; j < size; ++j){
+            distance[i * size + j] += distance_Bellman[j] - distance_Bellman[i];         
         }
     }
 
-    delete[] ticket;
-    delete[] visited;  
-
-    delete[] result_Bellman;
+    delete[] distance_Bellman;
     delete[] rowPtr_Arg;
     delete[] colIdx_Arg;
     delete[] value_Arg;
